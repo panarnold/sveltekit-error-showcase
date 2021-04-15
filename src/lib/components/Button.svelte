ecf6c7d7861c7b2e0ed8b67742fe3c677bc2790d
Arnold Cytrowski <cytrowskiarnold@gmail.com>
2021-04-15 11:55:22 +0200

    weird errors

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,21 @@
+<script>
+	// import { createEventDispatcher } from 'svelte';
+
+	// const dispatch = createEventDispatcher();
+
+	// function sayTyKurwoJebana() {
+	// 	dispatch('message', {
+	// 		text: 'siema kurwo jebana'
+	// 	});
+	// }
+</script>
+
+<button class="baton">
+	<slot>Jan papiesz drugi</slot>
+</button>
+
+<style lang="scss">
+	.baton {
+		@include grid-center-padding(15rem);
+	}
+</style>
